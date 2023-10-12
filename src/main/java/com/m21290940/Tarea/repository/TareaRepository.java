ad986ed7fa2a9a40e6f68bbea02341b318a9ca3c
Yael Alejandro Santana Michel <51036254+Ikigire@users.noreply.github.com>
2023-10-12 11:05:02 -0600

    Primer commit del microservicio de práctica de Tareas

---
+++ src/main/java/com/m21290940/Tarea/repository/TareaRepository.java
@@ -0,0 +1,7 @@
+package com.m21290940.Tarea.repository;
+
+import com.m21290940.Tarea.entities.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TareaRepository extends JpaRepository<Tarea, Long> {
+}
